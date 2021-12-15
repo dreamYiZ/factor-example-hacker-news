5cc0714e1b0ef21fe19dba7e9db04ab14de1f7de
Rentoo <dreamYiZ@outlook.com>
2021-12-15 23:04:07 +0800

    -

--- factor.config.ts
+++ factor.config.ts
@@ -1,7 +1,7 @@
 export default {
   variables: {
     FACTOR_APP_NAME: "FactorJS HN",
-    FACTOR_APP_EMAIL: "hi@factor.so",
-    FACTOR_APP_URL: "https://hn.factor.so",
+    FACTOR_APP_EMAIL: "dreamyiz@outlook.com",
+    FACTOR_APP_URL: "https://factorjs.herokuapp.com/",
   },
 }
